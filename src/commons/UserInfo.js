b06b4455a6afe67258fb36bafc3245ab4e14a108
DidierF <didier.fuentes@gmail.com>
2023-03-02 16:03:31 -0400

    Formatting.

--- src/commons/UserInfo.js
+++ src/commons/UserInfo.js
@@ -21,15 +21,15 @@ export default class UserInfo extends React.Component {
     showToastAlert: false,
     showBlockAlert: false,
     userAlreadyReported: this.props.author.is_reported_by_viewer,
-  }
+  };
 
   showBlockModal = () => {
     this.blockRef.current?.toggle();
-  }
+  };
 
   showBlockWarning = () => {
     this.warningRef.current?.toggle(this.props.author.display_name);
-  }
+  };
 
   onReportUser = () => {
     if (this.state.userAlreadyReported) {
@@ -43,15 +43,15 @@ export default class UserInfo extends React.Component {
         if (res.data.success) {
           this.setState({ showToastAlert: true });
           setTimeout(() => {
-            this.setState({ showToastAlert: false, userAlreadyReported: true  });
+            this.setState({ showToastAlert: false, userAlreadyReported: true });
           }, 2000);
         }
-      })
+      });
       return () => {
         controller.abort();
       };
     }
-  }
+  };
 
   onBlockUser = () => {
     const { request, controller } = blockUser(this.props.author.id);
@@ -66,7 +66,7 @@ export default class UserInfo extends React.Component {
     return () => {
       controller.abort();
     };
-  }
+  };
 
   render = () => {
     const { author, onHideUserInfo, isVisible, isDark, appColor } = this.props;
@@ -79,15 +79,23 @@ export default class UserInfo extends React.Component {
         supportedOrientations={['portrait', 'landscape']}
       >
         <View style={styles.background}>
-          <View style={styles.container} >
+          <View style={styles.container}>
             <View style={styles.infoContainer}>
               <View style={styles.header}>
                 <TouchableOpacity onPress={onHideUserInfo}>
-                  {x({ width: 20, height: 20, fill: isDark ? 'white' : 'black' })}
+                  {x({
+                    width: 20,
+                    height: 20,
+                    fill: isDark ? 'white' : 'black',
+                  })}
                 </TouchableOpacity>
                 <Text style={styles.name}>{author?.display_name}</Text>
                 <TouchableOpacity onPress={() => this.showBlockModal()}>
-                  {menuHSvg({ width: 23, height: 20, fill: isDark ? 'white' : 'black' })}
+                  {menuHSvg({
+                    width: 23,
+                    height: 20,
+                    fill: isDark ? 'white' : 'black',
+                  })}
                 </TouchableOpacity>
               </View>
               <AccessLevelAvatar
@@ -105,25 +113,42 @@ export default class UserInfo extends React.Component {
                 {'\n'}
                 <Text style={styles.yearSince}>
                   MEMBER SINCE{' '}
-                  {new Date(Date.now() - author.days_as_member * 86400000).getUTCFullYear()}
+                  {new Date(
+                    Date.now() - author.days_as_member * 86400000
+                  ).getUTCFullYear()}
                 </Text>
               </Text>
-              <View style={{ width: '100%', flexDirection: 'row', marginTop: 30 }}>
+              <View style={styles.rowsContainer}>
                 {[
-                  [author.xp, author.total_posts, author.days_as_member, author.total_post_likes],
-                  ['Total XP', 'Total posts', 'Days as a member', 'Total post likes'],
+                  [
+                    author.xp,
+                    author.total_posts,
+                    author.days_as_member,
+                    author.total_post_likes,
+                  ],
+                  [
+                    'Total XP',
+                    'Total posts',
+                    'Days as a member',
+                    'Total post likes',
+                  ],
                 ].map((array, i) => (
                   <View style={i ? { flex: 1 } : {}} key={`${i}`}>
                     {array.map((a, j) => (
                       <View style={styles.tableRow} key={`${i}${j}`}>
                         <Text
-                          style={{
-                            paddingVertical: 10,
-                            paddingLeft: i ? 10 : 15,
-                            color: i ? (isDark ? '#445f73' : 'black') : appColor,
-                            fontSize: 18,
-                            fontFamily: i ? 'OpenSans' : 'OpenSans-Bold',
-                          }}
+                          style={[
+                            styles.infoRow,
+                            {
+                              paddingLeft: i ? 10 : 15,
+                              color: i
+                                ? isDark
+                                  ? '#445f73'
+                                  : 'black'
+                                : appColor,
+                              fontFamily: i ? 'OpenSans' : 'OpenSans-Bold',
+                            },
+                          ]}
                         >
                           {a}
                         </Text>
@@ -134,22 +159,38 @@ export default class UserInfo extends React.Component {
               </View>
             </View>
           </View>
-          <BlockModal ref={this.blockRef} onReport={this.onReportUser} onBlock={this.showBlockWarning} />
+          <BlockModal
+            ref={this.blockRef}
+            onReport={this.onReportUser}
+            onBlock={this.showBlockWarning}
+          />
           <BlockWarningModal ref={this.warningRef} onBlock={this.onBlockUser} />
-          {this.state.showToastAlert &&
+          {this.state.showToastAlert && (
             <ToastAlert
-              content={this.state.userAlreadyReported ? "You have already reported this profile.":"The user profile was reported" }
-              icon={reportSvg({ height: 21.6, width: 21.6, fill: isDark ? 'black' : 'white' })}
+              content={
+                this.state.userAlreadyReported
+                  ? 'You have already reported this profile.'
+                  : 'The user profile was reported'
+              }
+              icon={reportSvg({
+                height: 21.6,
+                width: 21.6,
+                fill: isDark ? 'black' : 'white',
+              })}
               isDark={isDark}
             />
-          }
-          {this.state.showBlockAlert &&
+          )}
+          {this.state.showBlockAlert && (
             <ToastAlert
               content={`${author?.display_name} was blocked.`}
-              icon={banSvg({ height: 21.6, width: 21.6, fill: isDark ? 'black' : 'white' })}
+              icon={banSvg({
+                height: 21.6,
+                width: 21.6,
+                fill: isDark ? 'black' : 'white',
+              })}
               isDark={isDark}
             />
-          }
+          )}
         </View>
       </Modal>
     );
@@ -169,6 +210,15 @@ const setStyles = (isDark, appColor) =>
       flexWrap: 'wrap',
       paddingVertical: 30,
       justifyContent: 'center',
+    },
+    rowsContainer: {
+      width: '100%',
+      flexDirection: 'row',
+      marginTop: 30,
+    },
+    infoRow: {
+      paddingVertical: 10,
+      fontSize: 18,
     },
     container: {
       flex: 0.85,
